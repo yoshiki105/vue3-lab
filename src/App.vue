e49bd5aa8ffdb84b1b65c13167d4ddfe5f0c8a81
Yoshiki Hasumi <yoshiki.h.105@gmail.com>
2023-02-01 21:48:52 +0900

    Add: f-forで商品を複数表示

--- src/App.vue
+++ src/App.vue
@@ -4,29 +4,62 @@
     <h1>Vue.js ハンズオン</h1>
   </header>
   <main class="main">
-    <div class="item">
-      <div class="thumbnail">
-        <img :src="item.image" alt="">
+    <template
+      v-for="item in items"
+      :key="item.id"
+    >
+    <!-- <div class="items"> -->
+      <div class="item">
+        <div class="thumbnail">
+          <img :src="item.image" alt="">
+        </div>
+        <div class="description">
+          <h2>{{ item.name }}</h2>
+          <p>{{ item.description }}</p>
+          <span>¥<span class="price">{{ item.price }}</span></span>
+        </div>
       </div>
-      <div class="description">
-        <h2>{{ item.name }}</h2>
-        <p>{{ item.description }}</p>
-        <span>¥<span class="price">{{ item.price }}</span></span>
-      </div>
-    </div>
+    <!-- </div> -->
+    </template>
   </main>
 </template>
 
 <script setup>
 import { ref } from 'vue'
-const item = ref({
-  id: 1,
-  name: 'アボカドディップバケット',
-  description:
-    '刻んだ野菜をアボカドと混ぜてディップに。こんがり焼いたバゲットとお召し上がりください。',
-  price: 480,
-  image: '/images/item1.jpg'
-})
+const items = ref([
+  {
+    id: 1,
+    name: 'アボカドディップバケット',
+    description:
+      '刻んだ野菜をアボカドと混ぜてディップに。こんがり焼いたバゲットとお召し上がりください。',
+    price: 480,
+    image: '/images/item1.jpg'
+  },
+  {
+    id: 2,
+    name: 'あの日夢見たホットケーキ',
+    description:
+      '子供のころに食べたかった、あのホットケーキを再現しました。素朴でどこか懐かしい味をどうぞ。',
+    price: 1180,
+    image: '/images/item2.jpg'
+  },
+  {
+    id: 3,
+    name: 'HOP WTR',
+    description:
+      'ロサンゼルス生まれのスパークリングウォーター。ノンカロリー、ノンアルコールの新感覚飲料です。',
+    price: 320,
+    image: '/images/item3.jpg'
+  },
+  {
+    id: 4,
+    name: 'チーズフレンチフライ',
+    description:
+      'イタリア産チーズをたっぷりかけたアツアツのフレンチフライ。みんな大好きな一品です。',
+    price: 670,
+    image: '/images/item4.jpg'
+  }
+])
 </script>
 
 <style>
